35edb940bfcfb505e4bf9fdd0feeffa2822ff40a
Mohamed Hassan <mohamed7github@gmail.com>
2025-01-27 18:39:27 +0200

    - Removed CheckedModeBanner

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Smart Event Planner',
       theme: ThemeData(
         // This is the theme of your application.
