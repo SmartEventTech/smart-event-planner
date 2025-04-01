265ec8506f892f209d40e6b12cedcc1864ed1c71
Mohamed Hassan <mohamed7github@gmail.com>
2025-04-01 17:12:19 +0200

    refactor: remove unused imports and debug print statement in home screen

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:smart_event_planner/core/storage/secure_storage.dart';
 import 'package:smart_event_planner/shared/widgets/appBar/custom_appbar.dart';
 import 'package:smart_event_planner/features/home/presentation/screens/home_screen_body.dart';
 import 'package:smart_event_planner/features/home/presentation/widgets/chat_bot_floating_button.dart';
@@ -10,7 +8,6 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print("Home Screen + ${SecureStorage().getAccessToken().then((print))}");
     return Scaffold(
       appBar: CustomAppBar(),
       body: HomeScreenBody(),
